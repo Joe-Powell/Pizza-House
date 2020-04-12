c98abfff5f349d4c4f083df0adeb6e750cbb243e
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-04-12 00:03:25 -0500

    Create pizzaSelection.php

---
+++ pizzaSelection.php
@@ -0,0 +1,150 @@
+<?php include('inc/header.php');?>
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">  -->
+<style><?php include('pizzaSelection.css');?></style> 
+   
+
+<div class="all ">
+            <div class="card" style='width:200px; ' >
+                <img class="card-img-top" src="./images/pepperoni.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Pepperoni Pizza</h5>
+                  <p class="card-text">Enjoy this freshly prepared pepperoni and mozzarella cheese pizza </p>
+                  <button class="card-link pepperoni">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$8.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/cheese.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Cheese Pizza</h5>
+                  <p class="card-text">Enjoy this freshly prepared pepperoni and mozzarella cheese pizza </p>
+                  <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px;margin-top:8px;'>$6.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/basilcheese.jpg" alt="card example image" style='padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Basil Pizza</h5>
+                  <p class="card-text"> marinara sauce topped with Basil & mozzarella cheese. Order now! </p>
+                  <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$7.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+              <div class="card" style='width:200px;' >
+                <img class="card-img-top" src="./images/thickcrust.jpg" alt="card example image" style='width:120px; padding:10px; border-radius:20px;' height='100px' >
+                <div class="card-body">
+                  <h5 class="card-title">Thick Crust Pizza</h5>
+                  <p class="card-text">This gourmet pizza is ideal for easy late
+                    night delivery. Order now </p>                                                                                           
+                    <button class="card-link cheese">Order now</button><p style='display:inline-block; padding-left:10px; margin-top:8px;'>$9.99</p>
+                </div>
+              </div>
+            </div> 
+
+      <?php include('inc/footer.php'); ?>
