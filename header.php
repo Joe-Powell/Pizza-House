4e5f24e73e5e58cb1dbd438fc0f3146755b32fc1
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-04-11 23:58:00 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Project Pizza</title>
+
+    <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+    <link href='style3.css' type='text/css' rel='stylesheet'>
+    <script src="https://kit.fontawesome.com/871b467013.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <nav class="navCont">
+        <ul class='navbarUl'>
+            <li style='color:blue;' class='menuhover'>Menu</li>
+            <a href="/pizzaPlacePhp"><li>Home</li></a>
+            <li>Locations</li>
+            <h1 class='title-bignav'>Lawrence Pizza Shop</h1>
+        </ul>
+    </nav>
+
+    <i class="fas fa-bars"></i>
+    <h2 class='title-smallnav'>Pizza House</h2>
+
+<div class='menu-drop'>
+        <ul>
+            <a id='a' href='pizzaselection.php'><li class='anchLi'>Pizza</li></a>
+            <a id='a' href="#"><li class='anchLi'>Chicken</li></a>
+            <li>Pasta</li>
+            <li>Drinks</li>
+            <li>Sides</li>
+            <li>Desserts</li>
+        </ul>
+
+</div>
+
